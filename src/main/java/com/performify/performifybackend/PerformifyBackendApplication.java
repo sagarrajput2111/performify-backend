ca9aa74831cffb392e35454c0c6f6efdd8e31273
sagar <sagarrajput2111@gmail.com>
2025-01-26 01:40:42 +0530

    project setup

---
+++ src/main/java/com/performify/performifybackend/PerformifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.performify.performifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/performify/performifybackend/PerformifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.performify.performifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
